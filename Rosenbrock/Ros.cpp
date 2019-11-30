58dc2a2ad64d1d874fe751d61285d23298ff8186
dkarmit <dkarmit@yahoo.com>
2019-11-30 05:41:05 +0100

    Defined more Rosenbrock methods. Testing...

--- Rosenbrock/Ros.cpp
+++ Rosenbrock/Ros.cpp
@@ -5,7 +5,7 @@
 #include"Ros.hpp"
 #include "Jacobian/Jacobian.hpp"//this is treated as user input, since one may have an analytic form.
 
-
+/*--------------------------------------------------------------------------------------------------------*/
 class ROS3w{
     public:
         const int s=3;
@@ -31,6 +31,120 @@ class ROS3w{
             g[2][1]=-0.1537997822626;
         };
 };
+/*--------------------------------------------------------------------------------------------------------*/
+class ROS34PW2{
+    public:
+        const int s=4;
+        const int p=3;
+        double c[4];// c[i]=sum_{j} a[i][j]
+        double b[4]={2.4212380706095346e-1 , -1.2232505839045147 , 1.5452602553351020 , 4.3586652150845900e-1};
+        double bstar[4]={ 3.7810903145819369e-1 , -9.6042292212423178e-2 , 0.5 , 2.1793326075422950e-1};
+        double gamma=4.3586652150845900e-1; 
+        
+        double a[4][4] , g[4][4];
+
+        ~ROS34PW2(){};
+        ROS34PW2(){
+            
+            for (int i = 0; i < s; i++){c[i]=0; for (int j = 0; j < s; j++){  a[i][j]=0; g[i][j]=0; } }
+            
+            a[1][0]=8.7173304301691801e-1;
+            
+            a[2][0]=8.4457060015369423e-1;
+            a[2][1]=-1.1299064236484185e-1;
+           
+            a[3][2]=1.;
+
+            for (int i = 0; i < s; i++){for (int j = 0; j < s; j++){  c[i]+=a[i][j]; } }
+           
+
+            g[1][0]=-8.7173304301691801e-1;
+            
+            g[2][0]=-9.0338057013044082e-1;
+            g[2][1]=5.4180672388095326e-2;
+            
+            g[3][0]=2.4212380706095346e-1;
+            g[3][1]=-1.2232505839045147;
+            g[3][2]=5.4526025533510214e-1;
+           
+        };
+};
+/*--------------------------------------------------------------------------------------------------------*/
+class RODASP{
+    public:
+        const int s=6;
+        const int p=4;
+        double c[6];// c[i]=sum_{j} a[i][j]
+        double b[6]={ -8.0368370789e-2 , -5.6490613592e-2, 4.8828563004e-1 , 5.0571621148e-1 , -1.0714285714e-1 , 0.25};
+        double bstar[6]={ -1.7644376488 , -4.7475655721e-1 , 2.3696918469 , 6.1950235906e-1 , 0.25 , 0  };
+        double gamma=0.25; 
+        
+        double a[6][6] , g[6][6];
+
+        ~RODASP(){};
+        RODASP(){
+            
+            for (int i = 0; i < s; i++){c[i]=0; for (int j = 0; j < s; j++){  a[i][j]=0; g[i][j]=0; } }
+            a[1][0]=0.75;
+            
+            a[2][0]= 8.6120400814152190e-2; 
+            a[2][1]=0.1238795991858478;
+            
+            
+            a[3][0]= 0.7749345355073236;    
+            a[3][1] = 0.1492651549508680; 
+            a[3][2] = -0.2941996904581916;
+            
+            a[4][0]= 5.308746682646142;     
+            a[4][1] = 1.330892140037269;  
+            a[4][2] = -5.374137811655562; 
+            a[4][3]= -0.2655010110278497;
+
+            a[5][0]= -1.764437648774483;    
+            a[5][1] =-0.4747565572063027; 
+            a[4][2] = 2.369691846915802;
+            a[5][3]= 0.6195023590649829;    
+            a[5][4] = 0.25;
+
+            for (int i = 0; i < s; i++){for (int j = 0; j < s; j++){  c[i]+=a[i][j]; } }
+           
+
+            g[1][0]=-0.75;
+            
+            g[2][0]=-1.3551200000e-1;
+            g[2][1]=-1.3799200000e-1;
+            
+            g[3][0]=-1.2560800000;
+            g[3][1]=-2.5014500000e-1;
+            g[3][2]=1.2209300000;
+            
+            g[4][0]=-7.0731800000;
+            g[4][1]=-1.8056500000;
+            g[4][2]=7.7438300000;
+            g[4][3]=8.8500300000e-1;
+            
+            g[5][0]=1.6840700000;
+            g[5][1]=4.1826600000e-1;
+            g[5][2]=-1.8814100000;
+            g[5][3]=-1.1378600000e-1;
+            g[5][4]=-3.5714300000e-1;
+           
+           
+        };
+};
+/*--------------------------------------------------------------------------------------------------------*/
+
+
+
+
+
+
+
+
+
+
+
+
 // this is how the diffeq should look like
 #define n_eqs 3 //number of equations
 typedef double Array[n_eqs];//define an array type of length n_eqs
@@ -50,15 +164,17 @@ void sys( Array &lhs, Array &y  , double t )
         };
 
 #define initial_step_size 1e-4 
-#define minimum_step_size 1e-7
+#define minimum_step_size 1e-11 
 #define maximum_step_size 1e-2
 #define maximum_No_steps 1000000
-#define absolute_tolerance 1e-7
-#define relative_tolerance 1e-7
+#define absolute_tolerance 1e-12
+#define relative_tolerance 1e-12
 #define beta 0.85
 #define fac_max 3
 
-
+#define METHOD ROS34PW2
+// #define METHOD ROS3w // this is not very good...
+// #define METHOD RODASP // this is slow. I'll have to look at the paper to see what happens
 
 int main(int argc, const char** argv) {
     
@@ -70,7 +186,7 @@ int main(int argc, const char** argv) {
     Jacobian<diffeq,n_eqs> jac(sys);
 
     // this is not looing good.. Just pass the number of equations as argument to get rid of Array and Matrix...
-    Ros<diffeq,n_eqs, ROS3w,Jacobian<diffeq,n_eqs> > System(sys,y0, 
+    Ros<diffeq,n_eqs, METHOD ,Jacobian<diffeq,n_eqs> > System(sys,y0, 
      initial_step_size,  minimum_step_size,  maximum_step_size, maximum_No_steps, 
      absolute_tolerance, relative_tolerance, beta, fac_max);
     // System.next_step();
