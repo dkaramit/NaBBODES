4f0d7fc8e3ef11c5ef3fe84d75822f1199e21f33
dkarmit <dkarmit@yahoo.com>
2020-04-26 04:41:40 +0200

    Updated RKF. Uses vector for output.

--- RKF/RKF.hpp
+++ RKF/RKF.hpp
@@ -19,7 +19,6 @@ N_out number of output points (to be taken in intervals of approximately  1/(N_o
 #define _RKF_Func_ void  RKF<diffeq, N_eqs, RKF_method, N_out, LD>
 #define _RKF_Cosnt_ RKF<diffeq, N_eqs, RKF_method, N_out, LD>
 
-
 #include "RKF_costructor.hpp"
 #include "RKF_calc_k.hpp"
 #include "RKF_sums.hpp"
