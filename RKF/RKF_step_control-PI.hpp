b3aa0f51891a1d16784d1f8627a1935ea778d55d
dkarmit <dkarmit@yahoo.com>
2020-07-04 04:50:44 +0200

    changed the definition of scale in step control for RKF

--- RKF/RKF_step_control-PI.hpp
+++ RKF/RKF_step_control-PI.hpp
@@ -14,7 +14,7 @@ void RKF_Namespace::step_control(){
     LD fac=beta;
     
     for (int eq = 0; eq < N_eqs; eq++){
-        _sc=max(std::abs( ynext[eq] ), std::abs( ynext_star[eq] ));
+        _sc=max(std::abs( ynext[eq] ), std::abs( tmp_sol[eq] ));
         _sc=abs_tol+rel_tol*_sc;
         Delta+= std::pow((abs_delta[eq]/_sc),2.);
         
