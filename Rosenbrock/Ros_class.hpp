68e9b2a00129ace65d171c323d19c5ee2b497bab
dkaramit <dkaramit@yahoo.com>
2026-04-25 09:12:46 +0200

    added function that returns time an specific test

--- Rosenbrock/Ros_class.hpp
+++ Rosenbrock/Ros_class.hpp
@@ -123,6 +123,7 @@ class Ros{
         ~Ros()=default;
 
         const std::vector<LD>& get_t() const { return time; }
+        const std::vector<LD>& get_t(const unsigned int& step) const { return time.at(step); }
         //access the array of solution[eq]
         const std::vector<LD>& get_solution(const unsigned int& eq) const { return solution.at(eq); }
         //access the element solution[eq][step]
